7fef736a4ca872998dd62487aea7611991850698
pgiesin <pgiesin@gmail.com>
2022-04-15 15:54:01 -0600

    Configure PETAL

---
+++ assets/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    './js/**/*.js',
+    '../lib/*_web.ex',
+    '../lib/*_web/**/*.*ex',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,8 @@ config :igniteopedia_live, IgniteopediaLiveWeb.Endpoint,
   secret_key_base: "p1RcDsSVWecaBs4XGyfeoyKjFM62htOQ9N3iEnEih2cV6A0M8H59VQ1HoIT23OzS",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]},
   ]
 
 # ## SSL Support
